d8856566d47b7c89ea055be38fd67eabc0f42fde
ajacker <ajacker@foxmail.com>
2018-07-15 17:08:46 +0800

    修改了矿物生成概率

--- src/main/java/ajacker/nyacraft/worldgen/WorldGeneratorNyaOre.java
+++ src/main/java/ajacker/nyacraft/worldgen/WorldGeneratorNyaOre.java
@@ -11,14 +11,14 @@ import net.minecraftforge.event.terraingen.TerrainGen;
 import java.util.Random;
 
 public class WorldGeneratorNyaOre extends WorldGenerator {
-    private final WorldGenerator nyaoreGenerator = new WorldGenMinable(BlockLoader.nyaOre, 1);//1-2的随机整数
+    private final WorldGenerator nyaoreGenerator = new WorldGenMinable(BlockLoader.nyaOre, (int)(2+Math.random()*4));//2-6的随机整数
 
     @Override
     public boolean generate(World world, Random rand, int x, int y, int z) {
         if (TerrainGen.generateOre(world, rand, this, x, z, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
-            for (int i = 0; i < 4; ++i) {
+            for (int i = 0; i < 3; ++i) {
                 int posX = x + rand.nextInt(16);
-                int posY = 4+ rand.nextInt(20);
+                int posY = 6 + rand.nextInt(10);
                 int posZ = z + rand.nextInt(16);
                 System.out.println("生成矿物"+posX+","+posY+","+posZ);
                 BiomeGenBase biomeGenBase = world.getBiomeGenForCoords(posX, posZ);
